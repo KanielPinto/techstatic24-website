46b7c328ec1e9e507eec0fe1b032f0156823cc00
KanielPinto <kaniel.pinto@xaviers.edu.in>
2024-01-16 10:40:37 +0530

    gallery page added

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,11 +1,23 @@
-import { motion, AnimatePresence } from "framer-motion";
 import PageHeader from "@/components/PageHeader";
 import { TypeAnimation } from "react-type-animation";
-import Image from "next/image";
 import Footer from "@/components/Footer";
-import { useRouter } from "next/router";
+import Image from "next/image";
 import { getPosts } from "@/lib/api";
 import '@/styles/gallery.css'
+
+export async function getStaticProps(context) {
+    const posts = await getPosts();
+    return {
+
+        props: {
+            posts,
+        },
+        // Next.js will attempt to re-generate the page:
+        // - When a request comes in
+        // - At most once every second
+        revalidate: 20, // In seconds
+    };
+}
 
 export default function GalleryPage({ posts }) {
     console.log(posts);
@@ -29,25 +41,26 @@ export default function GalleryPage({ posts }) {
                         repeat={Infinity}
                         className="subtitle"
                     />
+
                     <div className="vertical-container">
 
-                        {posts?.slice(1, posts.length).map(({ imageLink, img_title, venue, date, description }) => {
-                                <div key={img_title} className="gallery-container">
-                                    <div className="img-container">
-                                        <div style={{ borderRadius: '25px', overflow: 'hidden' }}>
-                                            <Image src={imageLink} layout="responsive" width={400} height={300}></Image>
-                                        </div>
-                                    </div>
-                                    <div className="img-text-container">
-                                        <div className="img-title">{img_title}</div>
-                                        <div className="img-subtitle">{venue}, {date}</div>
-                                        <div className="img-desc">{description}</div>
+                        {posts.slice(1, posts.length).map(({ imageLink, img_title, venue, date, description }) => (
+                            <div key={img_title} className="gallery-container">
+                                <div className="img-container">
+                                    <div style={{ borderRadius: '25px', overflow: 'hidden' }}>
+                                        <Image src={imageLink} layout="responsive" width={400} height={300}></Image>
                                     </div>
                                 </div>
-                            })
+                                <div className="img-text-container">
+                                    <div className="img-title">{img_title}</div>
+                                    <div className="img-subtitle">{venue}, {date}</div>
+                                    <div className="img-desc">{description}</div>
+                                </div>
+                            </div>
+                        ))
                         }
-                    </div>     {/*vertical-container closes          */}
 
+                    </div>
                     <Footer></Footer>
                 </div>  {/*main-container closes          */}
 
@@ -58,16 +71,3 @@ export default function GalleryPage({ posts }) {
 }
 
 
-export async function getStaticProps(context) {
-    const posts = await getPosts();
-    return {
-
-        props: {
-            posts
-        },
-        // Next.js will attempt to re-generate the page:
-        // - When a request comes in
-        // - At most once every second
-        revalidate: 1, // In seconds
-    };
-}
